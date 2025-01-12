ada60ccf5976d428eab28fcc19b5c7bcf89e040f
ilya <i.churakov@rt-techpriemka.ru>
2025-01-12 18:30:01 +0300

    fixed linting

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { BuildPaths } from './../build/types/config';
-import { Configuration, DefinePlugin, RuleSetRule } from "webpack";
+import { Configuration, RuleSetRule } from "webpack";
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 
 export default ({config}: {config: Configuration}) => {
@@ -13,12 +13,6 @@ export default ({config}: {config: Configuration}) => {
     config.resolve?.modules?.push(paths.src)
     config.resolve?.extensions?.push('.ts', '.tsx')
 
-    // config.plugins?.push(
-    //     new DefinePlugin({
-    //         __IS_DEV__: true
-    //     })
-    // );
-    
     const fileLoaderRule = config.module?.rules?.find(
         // @ts-expect-error test
         (rule ) => rule && (rule as RuleSetRule).test instanceof RegExp && (rule as RuleSetRule).test.test('.svg')

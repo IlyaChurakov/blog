aa2b4e9a7413af6a643b3ac07246b8ed6447a2ce
ilya <i.churakov@rt-techpriemka.ru>
2025-03-07 00:21:01 +0300

    interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,16 @@
-import axios from 'axios';
 import { USER_LS_KEY } from 'shared/const/localstorage';
+import axios from 'axios';
 
 export const $api = axios.create({
   baseURL: __API__,
   headers: {
     authorization: `Bearer ${localStorage.getItem(USER_LS_KEY)}`,
   },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = `Bearer ${localStorage.getItem(USER_LS_KEY)}`;
+  }
+  return config;
 });
